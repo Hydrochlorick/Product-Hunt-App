9d7580f88f082891324cf195c9aca3918f2edbd6
Hydrochlorick <hydrochlorick@icloud.com>
2021-04-25 20:59:44 -0700

    Created Post Model

---
+++ ProductHuntPractice/Model/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  ProductHuntPractice
+//
+//  Created by Rick Jacobson on 4/25/21.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+}
