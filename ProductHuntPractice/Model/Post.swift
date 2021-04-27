a47bc5a7e145acb680ca18b3ce58d57e0838f485
Hydrochlorick <hydrochlorick@icloud.com>
2021-04-27 13:46:40 -0700

    Post model now JSON decodable

--- ProductHuntPractice/Model/Post.swift
+++ ProductHuntPractice/Model/Post.swift
@@ -13,4 +13,42 @@ struct Post {
     let tagline: String
     let votesCount: Int
     let commentsCount: Int
+    
+    let previewImageURL: URL
+}
+
+struct PostList: Decodable {
+    var posts: [Post]
+}
+
+extension Post: Decodable{
+    enum PostKeys: String, CodingKey {
+        // The variable names of the first three match the JSON object
+        case id
+        case name
+        case tagline
+        // These do not match, so they need to be mapped
+        case votesCount = "votes_count"
+        case commentsCount = "comments_count"
+        case previewImageURL = "screenshot_url"
+    }
+    
+    enum PreviewImageURLKeys: String, CodingKey {
+        // "screenshot_url" contains multiple entries for different resolutions, but we only want the 850px one
+        case imageURL = "850px"
+    }
+    
+    // But there's more work to do
+    init(from decoder: Decoder) throws {
+        let postsContainer = try decoder.container(keyedBy: PostKeys.self)
+        
+        id = try postsContainer.decode(Int.self, forKey: .id)
+        name = try postsContainer.decode(String.self, forKey: .name)
+        tagline = try postsContainer.decode(String.self, forKey: .tagline)
+        votesCount = try postsContainer.decode(Int.self, forKey: .votesCount)
+        commentsCount = try postsContainer.decode(Int.self, forKey: .commentsCount)
+        
+        let URLNestedContainer = try postsContainer.nestedContainer(keyedBy: PreviewImageURLKeys.self, forKey: .previewImageURL)
+        previewImageURL = try URLNestedContainer.decode(URL.self, forKey: .imageURL)
+    }
 }
