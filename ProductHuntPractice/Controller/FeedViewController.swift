a3bae31738d6143b365b35af10f1874e3c8240ec
Hydrochlorick <hydrochlorick@icloud.com>
2021-04-27 20:10:06 -0700

    TableView populates with feed from Product Hunt

--- ProductHuntPractice/Controller/FeedViewController.swift
+++ ProductHuntPractice/Controller/FeedViewController.swift
@@ -11,27 +11,34 @@ class FeedViewController: UIViewController {
 
     @IBOutlet weak var feedTableView: UITableView!
     
-    var mockData: [Post] = {
-       var meTube = Post(id: 0, name: "MeTube", tagline: "Stream videos for free!", votesCount: 25, commentsCount: 4)
-       var boogle = Post(id: 1, name: "Boogle", tagline: "Search anything!", votesCount: 1000, commentsCount: 50)
-       var meTunes = Post(id: 2, name: "meTunes", tagline: "Listen to any song!", votesCount: 25000, commentsCount: 590)
-
-       return [meTube, boogle, meTunes]
-    }()
+    var networkManager = NetworkManager()
+    var posts: [Post] = [] {
+        didSet {
+            feedTableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         feedTableView.dataSource = self
         feedTableView.delegate = self
+        
+        updateFeed()
     }
 
+    func updateFeed() {
+        networkManager.getPosts() { result in
+            // This is what we give to completion handler, which was given a list of posts as its parameter
+            self.posts = result
+        }
+    }
 
 }
 
 extension FeedViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mockData.count
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -41,7 +48,7 @@ extension FeedViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
         
-        let post = mockData[indexPath.row]
+        let post = posts[indexPath.row]
         
         // This will run the didSet function
         cell.post = post
